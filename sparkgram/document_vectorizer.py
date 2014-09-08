fd436c7413ec1e631c7396e34b8d0a8f30ff3a92
Rok Roskar <roskar@physik.uzh.ch>
2014-09-08 17:51:07 +0200

    added support for saving feature vectors to disk

--- sparkgram/document_vectorizer.py
+++ sparkgram/document_vectorizer.py
@@ -9,6 +9,7 @@ import re
 import sys, glob, os
 from random import shuffle
 import mmh3
+import numpy as np
 from pyspark.mllib.linalg import SparseVector
 from collections import defaultdict
 from nltk import word_tokenize
@@ -146,8 +147,8 @@ def ngram_frequency(text, ngram_range=[1,1], stop_words = None,
     d = defaultdict(int)
 
     # count the occurences
-    for ngram in word_ngrams(tokenizer(text), 
-                             ngram_range = ngram_range, 
+    for ngram in word_ngrams(tokenizer(text),
+                             ngram_range = ngram_range,
                              stop_words = stop_words) :
         d[ngram] += 1
 
@@ -183,41 +184,41 @@ class SparkDocumentVectorizer(object) :
     the ``read_docs`` method and perhaps also the ``doc_rdd`` method if your
     data ingestion is more complicated.
 
-    
+
     **Constructor inputs**:
-    
-    
+
+
     *sc* : Spark context
-    
+
     *doclist*: list of documents to process
-    
+
     **Optional Keywords**
-    
+
     *ngram_range*: a tuple with min, max ngrams to generate
-    
+
     *stop_words*: a list of stop words to use
-    
+
     *nmin/nmax*: minimum occurence count to use in filtering
-    
+
     *num_partitions*: the level of parallelism to use in spark for some
     of the processing steps
-    
+
     *feature_max*: maximum number of expected features. This sets the size of the
     sparse vectors generated during the document vectorization step and
     should be set to the nearest power of two larger than the expected
     number of features/ngrams
     """
-    
-    
+
+
     def __init__(self, sc, doclist,
                  ngram_range = [1,1], stop_words = None, nmin = None, nmax = None,
-                 num_partitions = None, features_max = 2**32, tokenizer = alpha_tokenizer) :
+                 num_partitions = None, features_max = None, tokenizer = alpha_tokenizer) :
 
         self._sc = sc
         self._ngram_range = ngram_range
         self._stop_words = stop_words
-        self._nmin = nmin 
-        self._nmax = nmax 
+        self._nmin = nmin
+        self._nmax = nmax
         self._num_partitions = num_partitions
         self._doclist = doclist
         self._features_max = features_max
@@ -248,32 +249,32 @@ class SparkDocumentVectorizer(object) :
         """
         return self.docvec_rdd
 
-    
+
     def apply_filter(self, filter_rdd = None, filter_func = None, **kwargs) :
         """
-        Applies the filtering to ngram_rdd  and regenerates the feature vectors. 
+        Applies the filtering to ngram_rdd  and regenerates the feature vectors.
 
         The new ngram_rdd is built using only the ngrams contained in ``filter_rdd``.
 
         **Optional Keywords**
-        
+
         *filter_rdd*: an RDD containing ngrams that will be used to construct a new ``ngram_rdd``
-        
-        *filter_func*: the function used to construct ``filter_rdd`` if ``filter_rdd`` is not given. 
+
+        *filter_func*: the function used to construct ``filter_rdd`` if ``filter_rdd`` is not given.
                        default is :func:`SparkDocumentVectorizer.filter_vocab`
 
-        **Keywords passed to ``filter_func``** 
-        
+        **Keywords passed to ``filter_func``**
+
         *nmin*: minimum occurence count
 
         *nmax*: maximum occurence count
 
         """
 
-        if filter_func is None : 
+        if filter_func is None :
             filter_func = self.filter_vocab
 
-        if filter_rdd is None : 
+        if filter_rdd is None :
             filter_rdd = filter_func(**kwargs)
 
         self._ngram_rdd = self.filter_by_rdd(filter_rdd)
@@ -281,32 +282,32 @@ class SparkDocumentVectorizer(object) :
         # docvec_rdd and vocab_rdd are both derived from ngram_rdd,
         # so force reevaluation
         del(self._docvec_rdd); self._docvec_rdd = None
-        del(self._vocab_rdd); self._vocab_rdd = None        
-    
+        del(self._vocab_rdd); self._vocab_rdd = None
 
-    def filter_by_rdd(self, filt_rdd) : 
+
+    def filter_by_rdd(self, filt_rdd) :
         """
-        Return a filtered ngram RDD based on ``nmin`` and ``nmax`` occurences of words 
+        Return a filtered ngram RDD based on ``nmin`` and ``nmax`` occurences of words
         in different documents.
         """
-        
+
         num_partitions, ngram_range, sw, tokenizer = self._num_partitions, self._ngram_range, self._stop_words, self._tokenizer
-        
+
         # generate an RDD of (ngram,context) pairs
-        ng_inv = self.doc_rdd.flatMap(lambda (x,y): 
+        ng_inv = self.doc_rdd.flatMap(lambda (x,y):
                                     [(ngram,x) for ngram in word_ngrams(tokenizer(y), ngram_range, sw)])
-        
+
         # do a join between the filtered vocabulary and the (ngram,context) RDD
         filtered_ngrams = filt_rdd.map(lambda x: (x,None)).join(ng_inv, num_partitions)
-        
+
         # invert the filtered ngram RDD to get (context, ngram) pairs, then group by context
         ngram_rdd = filtered_ngrams.map(lambda (x,y): (y[1], x)).groupByKey(num_partitions)
 
         # return the ngram_rdd but with counted occurences of ngrams
         return ngram_rdd.map(lambda (context,ngrams): SparkDocumentVectorizer.count_ngrams(context,ngrams))
-        
 
-    def filter_vocab(self, nmin = None, nmax = None) : 
+
+    def filter_vocab(self, nmin = None, nmax = None) :
         """
         Filter the vocabulary RDD by min,max occurence
         """
@@ -322,13 +323,13 @@ class SparkDocumentVectorizer(object) :
                        .map(lambda (x,_): x)
 
 
-    def get_corpus_frequency_rdd(self) : 
+    def get_corpus_frequency_rdd(self) :
         """
-        Return an RDD of ``(key,value)`` pairs, where ``key`` is the ngram and 
-        key is the number of documents that ngram appears in throughout the corpus. 
+        Return an RDD of ``(key,value)`` pairs, where ``key`` is the ngram and
+        key is the number of documents that ngram appears in throughout the corpus.
         """
         vocab_rdd = self.ngram_rdd.flatMap(lambda (_,x): [y[0] for y in x])
-        
+
         return vocab_rdd.map(lambda x: (x,1)).reduceByKey(lambda a,b : a+b, self._num_partitions)
 
 
@@ -339,7 +340,7 @@ class SparkDocumentVectorizer(object) :
         del(self._ngram_rdd)
         del(self._vocab_rdd)
         del(self._docvec_rdd)
-        
+
         self._ngram_rdd, self._vocab_rdd, self._docvec_rdd = None, None, None
 
 
@@ -347,18 +348,18 @@ class SparkDocumentVectorizer(object) :
     def read_docs(sc, doclist) :
         if type(doclist) is not list :
             raise RuntimeError("Please supply a list of filenames for processing")
-        
+
         return sc.parallelize(doclist)\
             .map(lambda x: open(x).read().lower())
-    
+
 
     @staticmethod
-    def count_ngrams(context, ngrams) : 
+    def count_ngrams(context, ngrams) :
         d = defaultdict(int)
 
-        for ngram in ngrams : 
+        for ngram in ngrams :
             d[ngram] += 1
-        
+
         return (context, [(ngram,d[ngram]) for ngram in d.keys()])
 
     #
@@ -386,15 +387,15 @@ class SparkDocumentVectorizer(object) :
         tokenizer = self._tokenizer
 
         if self._ngram_rdd is None :
-            if self._nmin is None and self._nmax is None : 
+            if self._nmin is None and self._nmax is None :
                 self._ngram_rdd = self.doc_rdd.mapValues(
                     lambda x: ngram_frequency(x, ngram_range,
                                               stop_words, tokenizer, features_max))
-            else : 
+            else :
                 self.apply_filter()
 
         return self._ngram_rdd
-    
+
 
     @property
     def vocab_rdd(self) :
@@ -402,7 +403,7 @@ class SparkDocumentVectorizer(object) :
         Extract the vocabulary from the ngram RDD
         """
         num_partitions, nmin, nmax = self._num_partitions, self._nmin, self._nmax
-        
+
         if self._vocab_rdd is None :
             # if no occurence filtering is required, get the distinct ngrams
             if nmin is None and nmax is None :
@@ -433,6 +434,9 @@ class SparkDocumentVectorizer(object) :
 
         features_max = self._features_max
 
+        if features_max is None :
+            features_max = next_power_of_two(self.vocab_rdd.count())
+
         if self._docvec_rdd is None :
             # The vectors are [[(metadata),[(ngram,ngram_ID),count],[...]]]
             # We want to have [[(metadata),SparseVector[(ngram_ID,count),...]]], i.e.
@@ -451,10 +455,145 @@ class SparkDocumentVectorizer(object) :
         return self._nfeatures
 
 
+    def get_vocab_map(self) :
+        """
+        Return an RDD of (ngram, hash) pairs
+        """
+        features_max = self._features_max
+        if features_max is None :
+            features_max = next_power_of_two(self.vocab_rdd.count())
+
+        return self.vocab_rdd.map(lambda x: (x,(mmh3.hash(x) & 0x7FFFFFFF) % features_max))
+
+    @staticmethod
+    def _write_single_partition_matrix(id, iterator, counts, datalen, path, filename, format):
+        """
+        Output the feature vectors from this partition into a file.
+        The matrix has to fit into worker memory.
+        """
+        import numpy as np
+        import scipy as sp
+        from scipy.sparse import csr_matrix
+
+        num_vectors = counts[id]
+        mydatalen = datalen[id]
+
+        # initialize data arrays
+        values = np.empty(mydatalen,'int32')
+        indices = np.empty(mydatalen,'int32')
+        lengths = np.empty(num_vectors,'int32')
+        indptr = np.zeros(num_vectors+1, 'int32')
+        contexts = np.ndarray(counts[id],dtype=np.object)
+
+        # get the data from the SparseVectors
+        n = 0
+        for i,(context, vec) in enumerate(iterator) :
+            l = len(vec.indices)
+            indices[n:n+l] = vec.indices
+            values[n:n+l] = vec.values
+            contexts[i] = context
+            lengths[i] = len(vec.values)
+            n += l
+
+        indptr[1:] = np.cumsum(lengths)
+
+        filename = '%s/%s_%d'%(path,filename,id)
+
+    	if format == 'numpy' :
+        	# write the numpy arrays to a file with
+        	np.savez('%s.npz'%(filename),values = values, indices = indices,
+                     indptr = indptr, contexts = contexts, datalen = mydatalen,
+                     num_vectors = num_vectors)
+
+        elif format == 'scipy' :
+            # output using matrix format
+            mat = csr_matrix((values,indices,indptr), shape = (len(iterator),vec.size))
+            scipy.io.mmwrite(filename,mat)
+        else :
+            raise RuntimeError("must specify format as either numpy or scipy")
+
+        yield 1
+
+
+    def write_feature_matrix(self, path, filename = 'docvec_data', format = 'numpy'):
+        """
+        Output the feature matrix into a file. The matrices
+        on individual workers have to fit into memory.
+        """
+        # cache this RDD in case it isn't already
+        self.docvec_rdd.cache()
+
+        # number of elements per partitions
+        counts = get_partition_counts(self.docvec_rdd)
+
+        # number of nonzero elements in all of the vectors
+        datalen = nonzero_vector_elements(self.docvec_rdd)
+
+        res = self.docvec_rdd.mapPartitionsWithIndex(
+                   lambda id, iterator: \
+                   SparkDocumentVectorizer\
+                   ._write_single_partition_matrix(id, iterator, counts, datalen,
+                                                   path, filename, format)).sum()
+
+        print 'created %d files %s/%s_[0-%d]'%(res,path,filename,res)
+
+
+def load_feature_matrix(path, filename = 'docvec_data', format = 'numpy') :
+    """Create a scipy sparse matrix from partition data saved on disk"""
+    import glob
+    from scipy.sparse import csr_matrix
+
+    files = glob.glob('%s/%s_*.npz'%(path,filename))
+    nparts = len(files)
+
+    # open up the data files
+    data = []
+    start_indices = np.zeros(nparts,dtype='int32')
+    total_datalen = 0
+    total_vectors = 0
+
+    # get the individual lengths and element offsets
+    for i,dat in enumerate((np.load(f) for f in files)) :
+        data.append(dat)
+        if i > 0 : start_indices[i] = data[i-1]['datalen']
+        total_datalen += dat['datalen']
+        total_vectors += dat['num_vectors']
+
+    start_indices = np.cumsum(start_indices)
+
+    values = np.ndarray(total_datalen,dtype='int32')
+    indices = np.ndarray(total_datalen,dtype='int32')
+    indptr = np.ndarray(total_vectors+1,dtype='int32')
+    contexts = np.ndarray(total_vectors,dtype=np.object)
+
+    nval = 0
+    nptr = 1 # the first item is zero
+    for i,dat in enumerate(data) :
+        lenval = dat['datalen']
+        lenptr = dat['num_vectors']
+
+        values[nval:nval+lenval] = dat['values']
+        indices[nval:nval+lenval] = dat['indices']
+        indptr[nptr:nptr+lenptr] = dat['indptr'][1:] + start_indices[i]
+        contexts[nptr-1:nptr+lenptr-1] = dat['contexts']
+
+        nval += lenval
+        nptr += lenptr
+
+    np.savez('%s/%s-all'%(path,filename),contexts=contexts, values=values, indices=indices, indptr=indptr)
+
+    return csr_matrix((values,indices,indptr))
+
 ### Some handy utility functions and stuff that is not used anymore
+def nonzero_vector_elements(rdd) :
+    """Returns non-zero element count on each partition"""
+    def helper(id,iterator):
+        yield (id,sum([len(vec.values) for (context,vec) in iterator]))
+    return rdd.mapPartitionsWithIndex(helper).collectAsMap()
+
 def get_partition_counts(rdd) :
     """Returns partition indices and item counts"""
-    return rdd.mapPartitionsWithSplit(_count_partitions).collectAsMap()
+    return rdd.mapPartitionsWithIndex(_count_partitions).collectAsMap()
 
 
 def _count_partitions(id,iterator):
