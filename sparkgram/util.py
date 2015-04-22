9fca5e9092fd8054c5acc195e02c5eb2e716a0fe
Rok Roskar <roskarr@ethz.ch>
2015-04-22 13:43:17 +0200

    working single pass variance and mean calculation

--- sparkgram/util.py
+++ sparkgram/util.py
@@ -28,15 +28,24 @@ class ColumnStats(object) :
         self._rdd = rdd
         self._mean = None
         self._norm = None
-        self._std = None
+        self._var = None
+        self._N = None
+
+    @property
+    def N(self):
+        rdd = self._rdd
+        if self._N is None : 
+            self._N = rdd.count()
+        return self._N
 
     @property
     def mean(self) :
         rdd = self._rdd
+        N = self.N
         if self._mean is None: 
             res = rdd.reduce(np.add)
             self._mean = res
-        return reshape_csr_to_array(self._mean)
+        return reshape_csr_to_array(self._mean)/N
 
     @property
     def norm(self) :
@@ -49,12 +58,83 @@ class ColumnStats(object) :
         return self._norm
 
     @property
-    def std(self) : 
+    def var(self) : 
         rdd = self._rdd
-        if self._std is None : 
+        if self._var is None : 
             mean = self.mean
+            N = self._N
+            self._var = rdd.map(lambda vec: np.frombuffer(vec - mean)**2).reduce(np.add)/N
+        return reshape_csr_to_array(self._var)
+    
+    @property
+    def std(self) : 
+        return np.sqrt(self.var)
             
-            
+def online_variance(stats, vec_iter) :
+    n, nnz, mean, M2 = stats['n'], stats['nnz'], stats['mean'], stats['M2']
+    
+    for vec in vec_iter :  
+        if type(vec) ==  csr_matrix : 
+            indices = vec.sorted_indices().indices
+            data = vec.sorted_indices().data        
+        elif type(vec) == np.ndarray: 
+            indices = vec.nonzero()
+            data = vec[indices]
+        else : 
+            raise RuntimeError('Data must be either scipy.sparse.csr_matrix or numpy.ndarray, but got %s instead'%(type(vec)))
+
+        n += 1
+        nnz[indices] += 1
+        delta = data - mean[indices]
+        mean[indices] += delta/nnz[indices]
+        M2[indices] += delta*(data - mean[indices])
+        print data, mean, delta, M2
+        
+    return ColumnStatDict({'n':n, 'nnz':nnz, 'mean':mean, 'M2':M2})
+
+def online_variance_agg(res1, res2) : 
+    res1['n'] += res2['n']
+    mean = res1['mean']
+    M2 = res1['M2']
+    
+    nnz = res1['nnz']
+    nnz +=  res2['nnz']
+    nnz_ind = nnz.nonzero() # use only non-zero elements to avoid division by zero
+
+    delta = res2['mean'] - res1['mean']
+    mean[nnz_ind] += delta[nnz_ind]*res2['nnz'][nnz_ind]/nnz[nnz_ind]
+    M2[nnz_ind] += res2['M2'][nnz_ind] + delta[nnz_ind]**2*res1['nnz'][nnz_ind]*res2['nnz'][nnz_ind]/nnz[nnz_ind]
+
+    res1['var'] = np.nan_to_num(M2/(nnz -1))
+
+    return res1
+    
+class ColumnStatDict(object) : 
+    def __init__(self, valsdict = None, size = None):
+        if valsdict is None : 
+            if size is None : raise RuntimeError('Size must be set')
+            valsdict = {'n':0,'nnz':np.zeros(size),'mean':np.zeros(size),'M2':np.zeros(size)}
+        self._myvals = valsdict
+        
+    def __getitem__(self, key) : 
+        return self._myvals[key]
+    
+    def __setitem__(self, key, value):
+        self._myvals[key] = value
+
+    @property
+    def std(self):
+        # need to scale M2 to account for the zeros that change the means
+        vals = self._myvals
+        n, nnz, mean, M2 = [vals[key] for key in ['n','nnz','mean','M2']]
+        M2_final = M2 + mean**2 * nnz**2 / (n**2 - n)
+        return np.sqrt(M2_final/(n-1))
+
+    @property
+    def mean(self):
+        return self['mean'] * self['nnz'] / self['n']
+
+
 def reshape_csr_to_array(csr) : 
     if csr.shape[1]*8 < (csr.data.nbytes + csr.indptr.nbytes + csr.indices.nbytes) : 
         csr = csr.toarray().squeeze()
